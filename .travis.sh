cd9bb90b984b739e7285e350f59de961ddac10a7
Didik Setiawan <ds@didiksetiawan.com>
2017-08-06 12:32:06 +0700

    Add Travis CI

---
+++ .travis.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -e
+
+BOOTSTRAP_OPTIONS=
+CONFIGURE_OPTIONS=()
+export CFLAGS="-O0 -g"
+
+test "$CC" = "clang" && export CXX="clang++"
+
+if [[ $TRAVIS_OS_NAME = 'osx' ]]; then
+	CONFIGURE_OPTIONS+=("")
+# else
+# 	CONFIGURE_OPTIONS+=("--enable-valgrind-tests")
+fi
+
+./bootstrap ${BOOTSTRAP_OPTIONS}
+./configure
+make -j3 check
+
+# for OPTS in "${CONFIGURE_OPTIONS[@]}"; do
+# 	./configure -C $OPTS
+# 	if test "$TRAVIS_OS_NAME" = 'osx'; then
+# 		# On OSX we are unable to find the Wget2 dylibs without
+# 		# installing first. However `make install` on linux systems
+# 		# fail due to insufficient permissions
+# 		make install -j3
+# 	fi
+# 	if make clean check -j3; then :; else
+# 		test -f src/testcurl/test-suite.log && cat src/testcurl/test-suite.log
+# 		exit 1
+# 	fi
+# done
+
+# make distcheck -j3
+
+# if [[ $CC = 'gcc' && $TRAVIS_OS_NAME = 'linux' ]]; then
+# 	make check-coverage
+# 	coveralls --include libwget/ --include src/ -e "libwget/<stdout>" -e lib/ -e tests/
+# fi
---
+++ .travis_setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [[ "$TRAVIS_OS_NAME" = "osx" ]]; then
+	brew update
+	brew install gnutls
+	brew install nettle
+	brew outdated autoconf || brew upgrade autoconf
+	brew outdated automake || brew upgrade automake
+	brew outdated libtool || brew upgrade libtool
+	brew install doxygen
+	brew outdated gettext || brew upgrade gettext
+	brew install flex
+	brew install libidn
+	brew install xz
+	brew install lbzip2
+	brew install lzip
+	brew install libgcrypt
+	brew link --force gettext
+# elif [[ "$TRAVIS_OS_NAME" = "linux" ]]; then
+# 	sudo apt-get -y install wget
+# 	wget http://ftp.gnu.org/gnu/libmicrohttpd/libmicrohttpd-0.9.55.tar.gz
+# 	tar zxf libmicrohttpd-0.9.55.tar.gz && cd libmicrohttpd-0.9.55/
+# 	./configure --prefix=/usr && make -j$(nproc) && sudo make install
+# 	pip install --user cpp-coveralls
+fi
